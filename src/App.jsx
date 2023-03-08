da1eb85c111a106e6566e7bcfd7c8b5ebd2984dc
Koki Yamaguchi <kiddiidid@gmail.com>
2023-03-08 11:17:02 -0800

    added flip card system

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,119 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import ReactCardFlip from 'react-card-flip';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+  const [flashcards, setFlashcards] = useState([
+    {
+      question: "人",
+      answer: "person",
+    },
+    {
+      question: "你好吗？",
+      answer: "How are you?",
+    },
+    {
+      question: "工作?",
+      answer: "work",
+    },
+    {
+      question:
+        "妈妈",
+      answer: "mother",
+    },
+    {
+      question:
+        "爸爸",
+      answer: "Father",
+    },
+
+    {
+      question: "谢谢",
+      answer: "Thank you",
+    },
+    {
+      question:
+        "晚安",
+      answer: "Good Night",
+    },
+    {
+      question:
+        "美国人",
+      answer: "American",
+    },
+    {
+      question:
+        "你是哪国人？ ",
+      answer: "where are  you from?",
+    },
+    {
+      question:
+        "很高兴认识你",
+      answer: "Nice to see you",
+    },
+  ]);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [showAnswer, setShowAnswer] = useState(false);
+
+  const handleNextCard = () => {
+    setCurrentIndex(Math.floor(Math.random() * flashcards.length));
+    setShowAnswer(false);
+  };
+
+  const handlePrevCard = () => {
+    setCurrentIndex(currentIndex - 1);
+    setShowAnswer(false);
+  };
+
+  const handleShowAnswer = () => {
+    setShowAnswer(true);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <div>
+      <h1>Chinese Character Practice!!</h1>
+      
 
-export default App
+      <ReactCardFlip isFlipped={showAnswer} flipDirection="horizontal">
+        <div className="card">
+          <div className="question-container">
+            <h3 className="question">
+              {flashcards[currentIndex].question}
+            </h3>
+          </div>
+          <div className="button-container">
+            <button className="show-answer-button" onClick={handleShowAnswer}>
+              Show Answer
+            </button>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="answer-container">
+            <h3 className="answer">
+              {flashcards[currentIndex].answer}
+            </h3>
+          </div>
+          <div className="button-container">
+            <button
+              className="next-button"
+              onClick={handleNextCard}
+              disabled={currentIndex === flashcards.length - 1}
+            >
+              Next
+            </button>
+            {currentIndex > 0 && (
+              <button className="prev-button" onClick={handlePrevCard}>
+                Previous
+              </button>
+            )}
+          </div>
+        </div>
+      </ReactCardFlip>
+    </div>
+  );
+};
+
+export default App;
